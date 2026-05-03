08ecd0d9a22c3e10e563e84c7b2aea4f9c998b6f
kdoyle99 <111944528+kdoyle99@users.noreply.github.com>
2026-05-03 16:23:46 -0400

    Weather section
    Added function for weather info and added global styles for it

--- index.js
+++ index.js
@@ -36,84 +36,84 @@ $(function() {
 });
 
 // Weather API
-// function getWeather(lat, long) {
-// 	// Grab #weather section to display weather information
-// 	let weatherSection = document.getElementById("weather");
+function getWeather() {
+	// Grab #weather section to display weather information
+	let weatherSection = document.getElementById("weather");
 
-// 	// Build output in empty string
-// 	let output = "";
+	// Build output in empty string
+	let output = "";
 
-// 	// Clear previous output
-// 	weatherSection.innerHTML = "";
+	// Clear previous output
+	weatherSection.innerHTML = "";
 
-// 	// Set location to Orlando, FL
-// 	let lat = 28.5384;
-// 	let long = -81.3789;
+	// Set location to Orlando, FL
+	let lat = 28.5384;
+	let long = -81.3789;
 
-// 	// Build url string to call API
-// 	let apiKey = "d5019f52f2aed7178c00da6f7c77cde3";
-// 	let imgUrlStart = "http://openweathermap.org/img/wn/";
-// 	let imgUrlEnd = "@2x.png";
-// 	let endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`;
+	// Build url string to call API
+	let apiKeyWeather = "d5019f52f2aed7178c00da6f7c77cde3";
+	let imgUrlStart = "http://openweathermap.org/img/wn/";
+	let imgUrlEnd = "@2x.png";
+	let endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${apiKeyWeather}`;
 
-// 	// XMLHttpRequest object
-// 	let xhr = new XMLHttpRequest();
-// 	// add an event listener for load event on the object
-// 	xhr.addEventListener("load", function(){
-// 		// for successful response, display weather
-// 		if(this.status == 200){
-// 			// log response to console
-// 			// console.log(this.response);
+	// XMLHttpRequest object
+	let xhr = new XMLHttpRequest();
+
+	// Event listener for load event on the object
+	xhr.addEventListener("load", function(){
+		// For successful response, display weather
+		if(this.status == 200){
+			// Get current date
+			let today = new Date();
+			let month = today.toLocaleString('default', { month: 'short' });
+			let day = today.getDate();
+			let dateFormat = `${month} ${day}`;
 			
-// 			// used to calculate actual date from ms given as date/time by the API
-// 			let ms = this.response.dt * 1000;
-// 			let date = new Date(ms);
+			// Get sunset time
+			ms = this.response.sys.sunset * 1000;
+			let set = new Date(ms);
 			
-// 			ms = this.response.sys.sunrise * 1000;
-// 			let rise = new Date(ms);
+			// Build url for weather icon/image
+			let iconCode = this.response.weather[0].icon;
+			let iconUrl = `${imgUrlStart}${iconCode}${imgUrlEnd}`;
 			
-// 			ms = this.response.sys.sunset * 1000;
-// 			let set = new Date(ms);
-			
-// 			// to build the url for weather icon/image
-// 			let iconCode = this.response.weather[0].icon;
-// 			let iconUrl = `${imgUrlStart}${iconCode}${imgUrlEnd}`;
-			
-// 			// append to the output string
-// 			output += `<h4>Today's Weather for ${this.response.name}</h4>
-// 								<img src="${iconUrl}" alt="${this.response.weather[0].main}">
-// 								<dl>
-// 									<dt>Current Conditions:</dt>
-// 									<dd>${this.response.weather[0].description}</dd>
-// 									<dt>Current Temp:</dt>
-// 									<dd>${Math.round(this.response.main.temp)}&deg;</dd>
-// 									<dt>Local Max Temp:</dt>
-// 									<dd>${Math.round(this.response.main.temp_max)}&deg;</dd>
-// 									<dt>Local Min Temp:</dt>
-// 									<dd>${Math.round(this.response.main.temp_min)}&deg;</dd>
-// 									<dt>Sunrise:</dt>
-// 									<dd>${new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'medium' }).format(rise)}</dd>
-// 									<dt>Sunset:</dt>
-// 									<dd>${new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'medium' }).format(set)}</dd>
-// 								</dl>`;
-// 			// 			// remove the class from the section that has been keeping it hidden on the page
-// 			weatherSection.classList.remove("hidden");
-// 			// add the class to make it display
-// 			weatherSection.classList.add("display");
-// 			// add the output string to that section to display our weather data from the API
-// 			weatherSection.innerHTML = output;
-// 		}else{
-// 			// 			// display an error message in the case where we get a 401 response from the server (which indicates an error in the call)
-// 			weatherSection.innerHTML = "There was an issue with your call to the Open Weather API. Check the endopint and try again.";
-// 		}
-// 	});
+			// Append to output string
+			output += 
+				`<h2>Come Rain or Shine</h2>
+				<h3>Weather for Today, ${dateFormat}</h3>
+				<p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path fill="#0d4c73" d="M0 188.6C0 84.4 86 0 192 0S384 84.4 384 188.6c0 119.3-120.2 262.3-170.4 316.8-11.8 12.8-31.5 12.8-43.3 0-50.2-54.5-170.4-197.5-170.4-316.8zM192 256a64 64 0 1 0 0-128 64 64 0 1 0 0 128z"/></svg>Orlando, FL</p>
+				<div>
+					<img src="${iconUrl}" alt="${this.response.weather[0].main}">
+					<p id="temp">${Math.round(this.response.main.temp)}&deg;</p>
+				</div>
+				<dl>
+					<dt>Current Conditions:</dt>
+					<dd>${this.response.weather[0].description}</dd>
+					<dt>Local Max Temp:</dt>
+					<dd>${Math.round(this.response.main.temp_max)}&deg;</dd>
+					<dt>Local Min Temp:</dt>
+					<dd>${Math.round(this.response.main.temp_min)}&deg;</dd>
+					<dt>Sunset:</dt>
+					<dd>${new Intl.DateTimeFormat('en-US', {timeStyle: 'short' }).format(set)}</dd>
+				</dl>`;
+
+			// Add the output string to #weather section to display weather data
+			weatherSection.innerHTML = output;
+		}else{
+			// Display error message if API data fails to load
+			weatherSection.innerHTML = "There was an issue with your call to the Open Weather API. Check the endopint and try again.";
+		}
+	});
 	
-// 	// set expected response type
-// 	xhr.responseType = "json";
+	// Expected response type
+	xhr.responseType = "json";
 	
-// 	// open connection to the endpoint of the correct type
-// 	xhr.open("GET", endpoint);
+	// Open connection to endpoint of correct type
+	xhr.open("GET", endpoint);
 	
-// 	// send request to the server
-// 	xhr.send();	
-// }
+	// Send request to server
+	xhr.send();	
+}
+
+// Call getWeather function
+getWeather();
